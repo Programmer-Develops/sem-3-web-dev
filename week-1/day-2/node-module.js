b5fa1394bd9d88cddca4782cbb94cb714b8c527f
Programmer-Develops <shachipandya29@gmail.com>
2026-08-05 11:48:47 +0530

    Add file system example: implement fs.readFile to read content from 1.txt

--- week-1/day-2/node-module.js
+++ week-1/day-2/node-module.js
@@ -9,3 +9,18 @@ const data = algo.update('Hello World'); // step 2: data is passed to the hash a
 const ans = data.digest('hex'); // step 3: the hash value is generated in hex format
 
 console.log(ans); // returns the hash value of the word "Hello World" using shake256 algorithm
+
+// 5. fs - file system
+
+const fs = require('fs');
+
+// a. read file
+
+fs.readFile("./1.txt", 'utf-8', (err, data) => {  // utf 8  is there for having the data in string format instead of buffer format
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(data);
+    }
+})
