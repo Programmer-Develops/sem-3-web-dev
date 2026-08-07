7d3e76c93965bd227a7269ad27cb5d98d10e1ec3
Programmer-Develops <shachipandya29@gmail.com>
2026-08-07 11:33:26 +0530

    learn: synchronous and asynchronous way of writing file

--- week-1/day-3/node-module.js
+++ week-1/day-3/node-module.js
@@ -10,23 +10,42 @@ const ans = data.digest('hex'); // step 3: the hash value is generated in hex fo
 
 console.log(ans); // returns the hash value of the word "Hello World" using shake256 algorithm
 
+
 // 5. fs - file system
 
 const fs = require('fs');
 
+
 // a. read file
 
 // utf 8  is there for having the data in string format instead of buffer format
-fs.readFile("./1.txt", 'utf-8', (err, data) => {  // It is asynchronous as it uses callback function which is executed after the file is read.
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log(data);
-    }
-})
+// fs.readFile("./1.txt", 'utf-8', (err, data) => {  // It is asynchronous as it uses callback function which is executed after the file is read.
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(data," => async");
+//     }
+// })
 
 // synchronous way of reading file
 
-const syncData = fs.readFileSync("./1.txt", 'utf-8'); // It is synchronous as it does not use callback function and the code execution is blocked until the file is read.
-console.log(syncData);
+// const syncData = fs.readFileSync("./1.txt", 'utf-8'); // It is synchronous as it does not use callback function and the code execution is blocked until the file is read.
+// console.log(syncData);
+
+
+// b. write file
+
+// fs.writeFile('./1.txt', 'Hello Synchronous World', (err)=> { // It is asynchronous as it uses callback function which is executed after the file is written.
+//     if (err) {
+//         console.log('error in writing file');
+//     } else {
+//         console.log('file written successfully');
+//     }
+// }); 
+
+// synchronous way of writing file
+
+// fs.writeFileSync('./1.txt', 'Hello Again Synchronous World'); // It is synchronous as it does not use callback function and the code execution is blocked until the file is written.
+
+
