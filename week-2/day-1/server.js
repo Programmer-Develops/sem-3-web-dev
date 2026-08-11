88eb6931e00232d0da3b7b03c83c896b86535d2c
Programmer-Develops <shachipandya29@gmail.com>
2026-08-11 11:04:56 +0530

    feat: add server functionality and initial test data

--- week-2/day-1/server.js
+++ week-2/day-1/server.js
@@ -1,16 +1,39 @@
-const process = require("process")
-const arguments = process.argv
+// const process = require("process")
+// const arguments = process.argv
 
-console.log(arguments)
+// console.log(arguments)
 
-const operation = +arguments[2]
-const a = +arguments[3]
-const b = +arguments[4]
+// const operation = +arguments[2]
+// const a = +arguments[3]
+// const b = +arguments[4]
 
-console.log(operation,typeof a, typeof b)
+// console.log(operation,typeof a, typeof b)
 
-function add(X,Y) {
-    console.log("Addition: ", X+Y)
-}
+// function add(X,Y) {
+//     console.log("Addition: ", X+Y)
+// }
 
-add(a,b)
+// add(a,b)
+
+
+//server
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=> {
+    if (req.url === '/') {
+        res.end('Hello World');
+    } else if (req.url == '/about') {
+        res.end('About Page');
+    } else if (req.url == '/data') {
+        const data = fs.readFileSync('./test.json', 'utf-8');
+        res.end(data);
+    } else {
+        res.end('404 Page Not Found');
+    }
+})
+
+server.listen(8080, ()=> {
+    console.log('Server is running on port 8080');
+})
