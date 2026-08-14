50ad7b887464553539b799240419247424fed3fd
Programmer-Develops <shachipandya29@gmail.com>
2026-08-14 11:47:24 +0530

    learn: internal module nodejs

--- week-2/day-4/1.js
+++ week-2/day-4/1.js
@@ -1,4 +1,19 @@
-const isEven = require("is-even");
+// 3 types of modules are there : 
+//      - core module 
+//      - internal module
+//      - external module
 
-console.log(isEven(4)); // true
-console.log(isEven(5)); // false
+
+// external module
+// const isEven = require("is-even");
+
+// console.log(isEven(4)); // true
+// console.log(isEven(5)); // false
+
+
+// internal module
+function greet() {
+    console.log("Hello World!")
+}
+
+module.exports = greet;
---
+++ week-2/day-4/2.js
@@ -0,0 +1,3 @@
+const greet  = require('./1.js')
+
+greet()
