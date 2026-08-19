8db798ede183be2b1300dfb78487dc178c66790f
Programmer-Develops <shachipandya29@gmail.com>
2026-08-19 11:30:41 +0530

    feat: add read route to fetch and parse JSON data from file

--- week-3/day-2/1.js
+++ week-3/day-2/1.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 
 app.get('/', (req,res)=> {
@@ -9,7 +10,15 @@ app.get('/about', (req,res)=> {
     res.send({msg:'Welcome to about'})
 })
 
-app.get('/home', (req,res)=> {
+app.get('/read', (req,res)=> {
+    const data = fs.readFileSync("./1.json", "utf-8")
+    // console.log(data, typeof data) // data is currently of string/json type
+    // console.log(data.student) // wont work and will give 'undefined' as it only works on object data type
+
+    const jsData = JSON.parse(data) // converting data to javascript object
+    console.log(jsData, typeof jsData) // now it will work as the data is converted to object
+    console.log(jsData.student)
+
     res.send({msg:'Welcome to home'})
 })
 
