94a20a985c68993654c7529b6f9a36f91c40d88b
Programmer-Develops <shachipandya29@gmail.com>
2026-08-19 11:55:37 +0530

    feat: add JSON parsing middleware and create route for student data

--- week-3/day-2/1.js
+++ week-3/day-2/1.js
@@ -2,6 +2,8 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 
+app.use(express.json)
+
 app.get('/', (req,res)=> {
     res.send({msg:'Welcome to server'})
 })
@@ -16,12 +18,29 @@ app.get('/read', (req,res)=> {
     // console.log(data.student) // wont work and will give 'undefined' as it only works on object data type
 
     const jsData = JSON.parse(data) // converting data to javascript object
-    console.log(jsData, typeof jsData) // now it will work as the data is converted to object
-    console.log(jsData.student)
+    console.log(jsData, typeof jsData) // data type now 'object'
+    console.log(jsData.student) // now it will work as the data is converted to object
+    
 
     res.send({msg:'Welcome to home'})
 })
 
+app.post('/create', (req,res)=>{
+    const payload = req.body;
+    console.log(payload)
+
+    const data = JSON.parse(fs.readFileSync('./1.json', 'utf-8'))
+
+    const stdata = data.student;
+    console.log(stdata);
+    student.push(stdata)
+    console.log(stdata);
+
+    data.student = stdata;
+
+    fs.writeFileSync('./1.json', JSON.stringify(data))
+    res.end({msg: "new stydent Created"})
+})
 app.listen(8080,()=>{
     console.log("Server Started")
 })
